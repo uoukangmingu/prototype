fdad01f55427ee0552c21b1c2979b5fd398c9081
mingu <mc12alsrn@gmail.com>
2024-10-21 02:48:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const audioPlayer1 = document.getElementById('audio-player-1');
     const audioPlayer2 = document.getElementById('audio-player-2');
 
-    // 시작 볼륨을 0으로 설정
-    audioPlayer1.volume = 0;
-    audioPlayer2.volume = 0;
-
     // 슬라이더 값에 따라 두 오디오 음량을 조절
     volumeSlider.addEventListener('input', function () {
         const volume = volumeSlider.value;
