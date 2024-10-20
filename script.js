1b7cea9184cf6dffda59d9b12de1bb1cdc6dbe75
mingu <mc12alsrn@gmail.com>
2024-10-21 02:15:22 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const volumeSlider = document.getElementById('volume-slider');
+    const volumeValue = document.getElementById('volume-value');
+    const audioPlayer = document.getElementById('audio-player');
+
+    // 슬라이더 값에 따라 오디오 음량을 조절
+    volumeSlider.addEventListener('input', function () {
+        const volume = volumeSlider.value;
+        volumeValue.textContent = volume;
+        audioPlayer.volume = volume / 100;
+    });
+});
